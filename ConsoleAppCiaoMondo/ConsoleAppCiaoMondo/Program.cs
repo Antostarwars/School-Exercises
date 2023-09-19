a3511c09432b6c78e5ce7ea14c7bcceda64f5e3c
Antostarwars <derosaantonio587@gmail.com>
2023-09-19 12:13:23 +0200

    First Project.
    Console App Hello World

---
+++ ConsoleAppCiaoMondo/ConsoleAppCiaoMondo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Antonio De Rosa 3H 2023-19-09
+namespace ConsoleAppCiaoMondo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press a key to close the Program.");
+            Console.ReadKey();
+        }
+    }
+}
