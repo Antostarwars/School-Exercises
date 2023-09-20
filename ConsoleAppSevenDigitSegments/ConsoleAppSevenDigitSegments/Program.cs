da12539e7a25335ad274bfcc2633ee426bd1038e
Antostarwars <derosaantonio587@gmail.com>
2023-09-20 08:57:51 +0200

    7 Digits Display

---
+++ ConsoleAppSevenDigitSegments/ConsoleAppSevenDigitSegments/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Antonio De Rosa 3H 2023-09-20
+namespace ConsoleAppSevenDigitSegments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+
+            Console.WriteLine("\n7 Segments Digit Display.\n");
+
+            Console.WriteLine("  ▓▓▓▓▓ ");
+            Console.WriteLine(" ▓     ▓");
+            Console.WriteLine(" ▓     ▓");
+            Console.WriteLine("  ▓▓▓▓▓ ");
+            Console.WriteLine(" ▓     ▓");
+            Console.WriteLine(" ▓     ▓");
+            Console.WriteLine("  ▓▓▓▓▓ ");
+
+
+            Console.WriteLine("\nPress a key to close the program.");
+            Console.ReadKey();
+        }
+    }
+}
