0393c6d6914df1b725c211a8c806a87948cf8b4a
Antostarwars <derosaantonio587@gmail.com>
2023-09-22 10:54:03 +0200

    Added TestProject Solution
    Created a Rectangle Surface Calculator.

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first side of the Rectangle. (cm)");
+            int firstSide = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second side value of the Rectangle. (cm)");
+            int secondSide = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(String.Format("the Rectangle Surface is {0}cm", firstSide * secondSide));
+        }
+    }
+}
