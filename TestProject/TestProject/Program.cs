2a49e0d8cbe01dc02e0d63869d400ea09758af1b
Antostarwars <derosaantonio587@gmail.com>
2023-09-29 10:54:33 +0200

    default

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Enter the first side of the Rectangle. (cm)");
             int firstSide = int.Parse(Console.ReadLine());
 
@@ -11,6 +12,7 @@
             int secondSide = int.Parse(Console.ReadLine());
 
             Console.WriteLine(String.Format("the Rectangle Surface is {0}cm", firstSide * secondSide));
+            
         }
     }
 }
---
+++ TestProject/TestProject2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestProject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            try
+            {
+                int.Parse(s);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Value was not a int.");
+            }
+
+            Console.WriteLine(string.Format("You have written: {0}", s));
+        }
+    }
+}
