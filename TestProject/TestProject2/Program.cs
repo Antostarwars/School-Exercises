1614ef9dc3b24bfc1f0f31e96133c3407ed2a9c8
Antostarwars <derosaantonio587@gmail.com>
2023-10-06 10:58:56 +0200

    Update Program.cs

--- TestProject/TestProject2/Program.cs
+++ TestProject/TestProject2/Program.cs
@@ -4,17 +4,49 @@
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
+            Console.Clear();
+            Console.Title = "Income Calculator by Antonio De Rosa 3H";
 
-            try
+            /*
+             * 10,000€ 15%
+             * oltre 10,000€ 20%
+             * 
+             * Se si superano i 10k€ i primi 10k vengono tassati al 15%
+             * il restante al 20%
+             * Esempio: 11k€ -> 10k al 15% e 1k al 20%
+             */
+
+            double grossIncome;
+            double taxes;
+            bool inputOk;
+
+            const double TAXES10K = 0.15;
+            const double TAXES15K = 0.20;
+
+            do
             {
-                int.Parse(s);
-            } catch (Exception e)
+                Console.WriteLine("How much Gross Income do you have?");
+                string input = Console.ReadLine();
+                inputOk = double.TryParse(input, out grossIncome);
+                if (!inputOk)
+                {
+                    Console.WriteLine("Gross Income it's not a valid currency.");
+                } else if (grossIncome < 0)
+                {
+                    Console.WriteLine("Gross Income can't be negative.");
+                }
+            } while (!inputOk);
+            
+            if (grossIncome <= 10000)
             {
-                Console.WriteLine("Value was not a int.");
+                taxes = grossIncome * TAXES10K;
+            } else
+            {
+                grossIncome -= 10000;
+                taxes = grossIncome * TAXES15K + 1500;
             }
 
-            Console.WriteLine(string.Format("You have written: {0}", s));
+            Console.WriteLine($"You have to pay {taxes}$ in taxes.");
         }
     }
 }
