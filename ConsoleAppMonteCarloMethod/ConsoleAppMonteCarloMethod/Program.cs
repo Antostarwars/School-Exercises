49deacbf22e05953afcdf82d68fcfc834dacf03e
Antostarwars <derosaantonio587@gmail.com>
2023-10-20 10:57:28 +0200

    montecarlo method

---
+++ ConsoleAppMonteCarloMethod/ConsoleAppMonteCarloMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleAppMonteCarloMethod
+{
+    internal class Program
+    {
+        const double SIDE = 1.0;
+        const long TOTAL_POINTS = 10000000000;
+
+        static void Main(string[] args)
+        {
+            Random rdn = new Random();
+
+            double squaredSide = Math.Pow(SIDE, 2);
+            int innerPoints = 0;
+            double stimatePI = 0;
+            for (int i = 0; i < TOTAL_POINTS; i++)
+            {
+                double x = rdn.NextDouble() * SIDE;
+                double y = rdn.NextDouble() * SIDE;
+
+                if (Math.Pow(x, 2) + Math.Pow(y, 2) <= squaredSide)
+                {
+                    innerPoints++;
+                }
+
+                stimatePI = 4.0 * ((double)innerPoints / (double)TOTAL_POINTS);
+            }
+
+            Console.WriteLine("Stimate PI is {0}", stimatePI);
+        }
+    }
+}
