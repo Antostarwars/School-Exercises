5f33f85dc3725aea9bff1f71a6a65df0e67091e7
Antostarwars <derosaantonio587@gmail.com>
2023-11-07 12:48:47 +0100

    Typing Test
    Typing test of a random sentence read in a file

---
+++ AdvancedConsole/AdvancedConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedConsole
+{
+    internal class Program
+    {
+        static void TestColorAndKeys(string[] args)
+        {
+            // Change Background Color and Foreground Color
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+          
+            char symbol = '#';
+            while (true)
+            {
+                Console.Write(symbol);
+                // Console.ReadKey(true); // Con parametro intercept == true non viene scritto sulla console il tasto premuto.
+                ReadKey();
+                char key = ReadKey();
+                if (key == 'x')
+                    symbol = '@';
+            }
+        }
+
+        static char ReadKey()
+        {
+            // Return the key pressed, or '\0' if no key is pressed
+            if (!Console.KeyAvailable) return '\0';
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            return key.KeyChar;
+        }
+
+        static void Main(String[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine("Press a key when you think 10 seconds are elapsed.");
+
+            stopwatch.Restart();
+            while (ReadKey() == '\0')
+            {
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine($"stopwatch.Elapsed = {stopwatch.Elapsed}");
+            Console.WriteLine($"stopwatch.ElapsedMilliseconds = {stopwatch.ElapsedMilliseconds}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TypingTest/TypingTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+// Antonio De Rosa 3H Typing Test Program
+// Typing Test
+namespace TypingTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Typing Test");
+            TypingTest(GetRandomSentence());
+        }
+
+        static void TypingTest(string sentence)
+        {
+            // Crea il timer
+            Stopwatch sw = new Stopwatch();
+            Console.Write(sentence);
+
+            Console.SetCursorPosition(0, 1);
+            sw.Restart();
+            int errors = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char key = Console.ReadKey(true).KeyChar;
+                // Check if the letter is correct and chnage the color
+                if (sentence[i] == key)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(sentence[i]);
+                }
+                else
+                {
+                    errors++;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(sentence[i]);
+                }
+            }
+            // Stop the Timer
+            sw.Stop();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Output Time, CPM and Errors
+            Console.WriteLine($"\nTime Elapsed: {sw.Elapsed}");
+            Console.WriteLine($"Characters per Minute: {(int)(sentence.Length / ((double)sw.ElapsedMilliseconds / 60000.0))}");
+            Console.WriteLine($"Errors: {errors}");
+
+            Console.ReadKey();
+        }
+
+        static string GetRandomSentence()
+        {
+            // Read from a file all the sentences
+            string filePath = @"..\..\sentences.txt";
+            List<string> lines = new List<string>();
+
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+
+            // Return a random sentence from the list
+            return lines[new Random().Next(0, lines.Count)];
+        }
+    }
+}
