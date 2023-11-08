735d4c905964af2d7bdced91e689c1101ed1ce94
Antostarwars <derosaantonio587@gmail.com>
2023-11-08 09:47:31 +0100

    EnumTest

---
+++ EnumTest/EnumTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // New Data Type, it works like an int. 0,1,2 etc
+        enum Test
+        {
+            RED = 0,
+            GREEN
+        }
+    }
+}
