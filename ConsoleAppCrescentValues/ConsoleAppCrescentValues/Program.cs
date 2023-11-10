f1cbe7c7730c425f76ace4f33b78572aa7e60b29
Antostarwars <derosaantonio587@gmail.com>
2023-11-10 10:57:18 +0100

    numeri in ordine crescenti

---
+++ ConsoleAppCrescentValues/ConsoleAppCrescentValues/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+
+// Antonio De Rosa 3H 2023-11-10
+/*
+ * Write in output crescent order of the value in input.
+ */
+namespace ConsoleAppCrescentValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert 3 Integer Numbers:");
+            int a = int.Parse(Console.ReadLine());         
+            int b = int.Parse(Console.ReadLine());         
+            int c = int.Parse(Console.ReadLine());         
+            int temp;                                      
+                                                           
+            while (!checkCrescent(a,b,c))                  
+            {                                              
+                if (c > b)                                 
+                {                                          
+                    temp = b;                              
+                    b = c;                                 
+                    c = temp;                              
+                }                                          
+                if (b > a)                                 
+                {                                          
+                    temp = a;                              
+                    a = b;                                 
+                    b = temp;                              
+                }                                          
+            } // it's possible to not use the while loop using 3 if
+                                                           
+            Console.WriteLine("Numbers in crescent value order are: {0}, {1}, {2}", c, b, a);
+            Console.ReadKey();                             
+        }                                                  
+                                                           
+        static bool checkCrescent(int a, int b, int c)     
+        {                                                  
+            if (a < b || b < c) return false;              
+            if (b < c) return false;
+            return true;                                   
+        }                                                  
+    }                                                      
+}                                                          
