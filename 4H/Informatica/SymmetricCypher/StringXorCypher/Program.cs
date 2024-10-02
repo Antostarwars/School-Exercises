e6bf67d1b3aba7dd3612ff90acdd869b0efd3534
Antostarwars <derosaantonio587@gmail.com>
2024-10-02 12:49:34 +0200

    Added SymmetricCypher Project

---
+++ 4H/Informatica/SymmetricCypher/StringXorCypher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace StringXorCypher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string msg = "Hey! Meet me at 8pm on the 13th street.";
+                string key = "That's the key for the encryption, kekw";
+
+                string encryptedString = SymmetricCypher.Encrypt(msg, key);
+            }
+
+            {
+                string msg = "Hey! Meet me at 8pm on the 13th street.";
+                SymmetricCypher cypher = new SymmetricCypher("That's the key for the encryption, kekw");
+
+                string encryptedString = cypher.Encrypt(msg);
+            }
+        }
+    }
+}
