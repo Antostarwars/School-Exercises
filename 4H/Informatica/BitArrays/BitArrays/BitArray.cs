f20db3e9908da6dc2bbfd6ef1fb827a7dd983088
Antostarwars <derosaantonio587@gmail.com>
2024-10-09 12:54:19 +0200

    Finished BitArray Project and Sieve of Eratosthenes

--- 4H/Informatica/BitArrays/BitArrays/BitArray.cs
+++ 4H/Informatica/BitArrays/BitArrays/BitArray.cs
@@ -1,8 +1,63 @@
-﻿namespace BitArrays
+﻿using System.Numerics;
+
+namespace BitArrays
 {
     class BitArray
     {
         private const int BITS_PER_VALUE = 32;
         private uint[] bits;
+        private long bitsNumber;
+
+        public BitArray(long bitsNumber, bool initial_value)
+        {
+            long n_uints = bitsNumber / BITS_PER_VALUE;
+            if (bitsNumber % BITS_PER_VALUE != 0)
+                ++n_uints;  // serve un (uint) in più per i bits residui
+
+            if (n_uints > int.MaxValue - 60)  // il valore massimo è stato trovato per tentativi
+                throw new OverflowException("Too many bits");
+
+            this.bits = new uint[n_uints];
+            this.bitsNumber = bitsNumber;
+            SetAllBits(initial_value);
+        }
+
+        public BitArray(long bitsNumber) : this(bitsNumber, false) { }
+
+        public void SetAllBits(bool value)  // imposta tutti i bit a true o false
+        {
+            uint _value = 0;
+            if (value)
+                _value = ~_value;
+
+            for (int i = 0; i < bits.Length; ++i)
+                bits[i] = _value;
+        }
+
+        public bool GetBit(long bit_index)
+        {
+            int uint_idx = (int)(bit_index / BITS_PER_VALUE);
+            int bit_idx = (int)(bit_index % BITS_PER_VALUE);
+
+            return (bits[uint_idx] & (1 << bit_idx)) != 0;
+        }
+        public void SetBit(long bit_index, bool value)
+        {
+            int uint_idx = (int)(bit_index / BITS_PER_VALUE);
+            int bit_idx = (int)(bit_index % BITS_PER_VALUE);
+
+            uint mask = (uint)1 << bit_idx;
+            if (value)
+                bits[uint_idx] |= mask;
+            else
+                bits[uint_idx] &= ~mask;  // ~ è un not-bitwise (inverte gli 0 e gli 1 della mask)
+        }
+
+
+        public bool this[long index]
+        {
+            get => GetBit(index);
+            set => SetBit(index, value);
+        }
     }
 }
--- 4H/Informatica/BitArrays/BitArrays/Program.cs
+++ 4H/Informatica/BitArrays/BitArrays/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace BitArrays
+﻿using System.Diagnostics;
+
+namespace BitArrays
 {
     internal class Program
     {
+        static List<long> EratosthenesSieve(long max_value)
+        {
+            List<long> primes = new List<long>();
+
+            BitArray sieve = new BitArray(max_value + 1, true);
+            for (long n = 2; n <= max_value; ++n)
+            {
+                if (sieve[n])  // se true, allora n è primo
+                {
+                    primes.Add(n);
+                    for (long mult_n = (n << 1); mult_n <= max_value; mult_n += n)  // genera tutti i multipli di n, che vanno marcati come non-primi
+                    {
+                        sieve[mult_n] = false;
+                    }
+                }
+            }
+
+            return primes;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var timer = Stopwatch.StartNew();
+            List<long> primes = EratosthenesSieve(100);
+            timer.Stop();
+
+            Console.WriteLine($"Found {primes.Count:N} primes in {timer.ElapsedMilliseconds} ms");
+            foreach (long n in primes)
+                Console.Write($"{n}, ");
+            Console.WriteLine();
         }
     }
 }
