f25129efad58849c16a46a3c80a33ebe45cf8dae
Antostarwars <derosaantonio587@gmail.com>
2024-11-15 10:51:01 +0100

    Dijkstra Project OOP

---
+++ 4H/Informatica/DijkstraOOP/DijkstraOOP/Grafi.cs
@@ -0,0 +1,191 @@
+﻿/*
+ * Implementazione di Grafi Diretti e Non Diretti usando la programmazione ad oggetti
+ * Progetto di Antonio De Rosa 4H 2024-11-15
+ */ 
+namespace DijkstraOOP
+{
+    public class GrafoDiretto
+    {
+        public struct Arco
+        {
+            public int nodo_partenza;
+            public int nodo_arrivo;
+            public int costo;
+        }
+
+        private int numeroNodi;
+        private int numeroMaxNodi;
+        private List<string> nodi;
+        private List<Arco> archi;
+
+        /// <summary>
+        /// Costruisce la classe, è garantito che non si supererà numero_max_nodi.
+        /// </summary>
+        /// <param name="numero_max_nodi">Numero massimo di nodi.</param>
+        public GrafoDiretto(int numero_max_nodi = 1000)
+        {
+            this.numeroMaxNodi = numero_max_nodi;
+            this.nodi = new List<string>(numeroMaxNodi);
+            this.archi = new List<Arco>();
+        }
+
+        /// <summary>
+        /// Proprietà che torna il numero attuale di nodi inseriti.
+        /// </summary>
+        public int NumeroNodi { get { return nodi.Count; } }
+
+        /// <summary>
+        /// Aggiunge un nodo.
+        /// </summary>
+        /// <param name="nome">Nome del nodo.</param>
+        public void AggiungiNodo(string nome)
+        {
+            if (nodi.Count >= numeroMaxNodi) throw new Exception($"You cannot add more than {numeroMaxNodi} nodes.");
+            nodi.Add(nome);
+        }
+
+        /// <summary>
+        /// Aggiunge un arco fra i due nodi dati, con il costo indicato.
+        /// </summary>
+        /// <param name="nome_nodo_partenza">Nome del nodo di partenza.</param>
+        /// <param name="nome_nodo_arrivo">Nome del nodo di arrivo.</param>
+        /// <param name="costo">Costo dell'arco.</param>
+        public virtual void AggiungiArco(string nome_nodo_partenza, string nome_nodo_arrivo, int costo)
+        {
+            Arco arco;
+            arco.costo = costo;
+            int idxPartenza = this[nome_nodo_partenza];
+            int idxArrivo = this[nome_nodo_arrivo];
+            if (idxPartenza == -1 || idxArrivo == -1) throw new IndexOutOfRangeException("L'indice di partenza o di arrivo non è valido.");
+            arco.nodo_partenza = idxPartenza;
+            arco.nodo_arrivo = idxArrivo;
+
+            archi.Add(arco);
+        }
+
+        /// <summary>
+        /// Aggiunge un arco fra i due nodi dati, con il costo indicato.
+        /// </summary>
+        /// <param name="arco">Struttura dell'arco da aggiungere.</param>
+        public virtual void AggiungiArco(Arco arco)
+        {
+            archi.Add(arco);
+        }
+
+        /// <summary>
+        /// Indicizzatore per i nodi.
+        /// </summary>
+        /// <param name="nodo">Indice del nodo.</param>
+        /// <returns>Nome del nodo.</returns>
+        public string this[int nodo]
+        {
+            get { return nodi[nodo]; }
+        }
+
+        /// <summary>
+        /// Indicizzatore per i nodi.
+        /// </summary>
+        /// <param name="nome_nodo">Nome del nodo.</param>
+        /// <returns>Indice del nodo.</returns>
+        public int this[string nome_nodo]
+        {
+            get
+            {
+                for (int i = 0; i < nodi.Count; i++)
+                {
+                    if (nodi[i] == nome_nodo) return i;
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Enumeratore per gli archi uscenti dal nodo dato.
+        /// </summary>
+        /// <param name="nodo">Indice del nodo.</param>
+        /// <returns>Archi uscenti dal nodo.</returns>
+        public IEnumerable<Arco> ArchiUscenti(int nodo)
+        {
+            foreach (var arco in archi)
+            {
+                if (arco.nodo_partenza == nodo)
+                {
+                    yield return arco;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Enumeratore per gli archi entranti nel nodo dato.
+        /// </summary>
+        /// <param name="nodo">Indice del nodo.</param>
+        /// <returns>Archi entranti nel nodo.</returns>
+        public IEnumerable<Arco> ArchiEntranti(int nodo)
+        {
+            foreach (var arco in archi)
+            {
+                if (arco.nodo_arrivo == nodo)
+                {
+                    yield return arco;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Controlla se l'indice del nodo è valido.
+        /// </summary>
+        /// <param name="nodo">Indice del nodo.</param>
+        /// <returns>True se l'indice è valido, altrimenti false.</returns>
+        public bool ControllaIndiceNodo(int nodo)
+        {
+            try
+            {
+                string BinObj = nodi[nodo];
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    public class GrafoNonDiretto : GrafoDiretto  // specializzazione di GrafoDiretto per grafi "non diretti" (se si può andare da A a B con costo 10, allora si va anche da B ad A con lo stesso costo)
+    {
+        /// <summary>
+        /// Costruisce la classe, è garantito che non si supererà numero_max_nodi.
+        /// </summary>
+        /// <param name="numero_max_nodi">Numero massimo di nodi.</param>
+        public GrafoNonDiretto(int numero_max_nodi = 1000) : base(numero_max_nodi) { }
+
+        /// <summary>
+        /// Aggiunge un arco fra i due nodi dati, con il costo indicato.
+        /// </summary>
+        /// <param name="nome_nodo_partenza">Nome del nodo di partenza.</param>
+        /// <param name="nome_nodo_arrivo">Nome del nodo di arrivo.</param>
+        /// <param name="costo">Costo dell'arco.</param>
+        public override void AggiungiArco(string nome_nodo_partenza, string nome_nodo_arrivo, int costo)
+        {
+            base.AggiungiArco(nome_nodo_partenza, nome_nodo_arrivo, costo);
+            base.AggiungiArco(nome_nodo_arrivo, nome_nodo_partenza, costo);
+        }
+
+        /// <summary>
+        /// Aggiunge un arco fra i due nodi dati, con il costo indicato.
+        /// </summary>
+        /// <param name="arco">Struttura dell'arco da aggiungere.</param>
+        public override void AggiungiArco(Arco arco)
+        {
+            base.AggiungiArco(arco);
+
+            // Creo il secondo arco.
+            int idxTemp = arco.nodo_partenza;
+            arco.nodo_partenza = arco.nodo_arrivo;
+            arco.nodo_arrivo = idxTemp;
+
+            base.AggiungiArco(arco);
+        }
+    }
+}
---
+++ 4H/Informatica/DijkstraOOP/DijkstraOOP/Program.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * Implementazione di Grafi Diretti e Non Diretti usando la programmazione ad oggetti
+ * Progetto di Antonio De Rosa 4H 2024-11-15
+ */
+namespace DijkstraOOP
+{
+    internal class Program
+    {
+        static bool Dijkstra(GrafoDiretto g, int nodo_partenza, int nodo_arrivo, List<int> path, List<int> dist)
+        {
+            if (!g.ControllaIndiceNodo(nodo_partenza))
+                throw new Exception("Nodo di partenza non valido");
+
+            if (nodo_arrivo >= 0 && !g.ControllaIndiceNodo(nodo_arrivo))
+                throw new Exception("Nodo di arrivo non valido");
+
+            // STEP 1 : inizializzazione
+            List<int> V = new List<int>();
+
+            path.Clear();
+            path.InsertRange(0, Enumerable.Repeat(-1, g.NumeroNodi));
+
+            dist.Clear();
+            dist.InsertRange(0, Enumerable.Repeat(int.MaxValue, g.NumeroNodi));
+            dist[nodo_partenza] = 0;
+
+            while (true)
+            {
+                // STEP 2 : ricerca del nodo da visitare
+                int nodo_min = -1;
+                for (int nodo = 0; nodo < g.NumeroNodi; ++nodo)
+                {
+                    if (V.Contains(nodo))  // salta i nodi già visitati
+                        continue;
+                    if (nodo_min < 0 || dist[nodo_min] > dist[nodo])
+                        nodo_min = nodo;
+                }
+
+                // STEP 3 : condizione di uscita
+                if (nodo_min < 0)
+                {
+                    // tutti i nodi sono stati visitati!
+                    // Questa situazione dovrebbe capitare solo se nodo_arrivo < 0 e,
+                    // in questo caso, l'algoritmo procede fino ad etichettare tutti i nodi
+                    return (V.Count == g.NumeroNodi);
+                }
+                if (dist[nodo_min] == int.MaxValue)
+                    return false;  // il grafo non è interconnesso (manca il persorso da nodo_partenza a nodo_arrivo)
+
+                // STEP 4 : visita il nodo nodo_min
+                V.Add(nodo_min);
+                foreach (GrafoDiretto.Arco a in g.ArchiUscenti(nodo_min))
+                {
+                    if (V.Contains(a.nodo_arrivo))  // se l'arco è verso un nodo già espanso, occorre ignorare
+                        continue;
+
+                    int nuova_dist = dist[nodo_min] + a.costo;
+                    if (dist[a.nodo_arrivo] > nuova_dist)  // aggiorna solo se il valore è migliorativo
+                    {
+                        dist[a.nodo_arrivo] = nuova_dist;
+                        path[a.nodo_arrivo] = nodo_min;
+                    }
+                }
+
+                // STEP 5 : condizione di terminazione
+                if (nodo_min == nodo_arrivo)
+                    return true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antonio De Rosa 4H 2024-11-15");
+            GrafoNonDiretto g = new GrafoNonDiretto();
+
+            g.AggiungiNodo("A");  // 0
+            g.AggiungiNodo("B");  // 1
+            g.AggiungiNodo("C");  // 2
+            g.AggiungiNodo("D");  // 3
+            g.AggiungiNodo("E");  // 4
+            g.AggiungiNodo("F");  // 5
+            g.AggiungiNodo("G");  // 6
+            g.AggiungiNodo("H");  // 7
+
+            g.AggiungiArco("A", "B", 5);
+            g.AggiungiArco("A", "C", 2);
+            g.AggiungiArco("B", "C", 3);
+            g.AggiungiArco("B", "G", 3);
+            g.AggiungiArco("C", "D", 1);
+            g.AggiungiArco("C", "E", 3);
+            g.AggiungiArco("C", "G", 5);
+            g.AggiungiArco("D", "E", 1);
+            g.AggiungiArco("D", "F", 3);
+            g.AggiungiArco("E", "F", 6);
+            g.AggiungiArco("E", "H", 4);
+            g.AggiungiArco("F", "H", 1);
+
+            string nodo_partenza = "A";
+            string nodo_arrivo = "H";
+
+            List<int> path = new List<int>();
+            List<int> dist = new List<int>();
+            bool esito = Dijkstra(g, g[nodo_partenza], g[nodo_arrivo], path, dist);
+            if (esito)
+            {
+                string percorso = "";
+                for (int nodo = g[nodo_arrivo]; nodo >= 0; nodo = path[nodo])
+                {
+                    if (nodo != g[nodo_arrivo])
+                        percorso = " -> " + percorso;
+                    percorso = g[nodo] + percorso;
+                }
+                Console.WriteLine($"Il percorso migliore da '{nodo_partenza}' verso '{nodo_arrivo}' è {percorso}");
+                Console.WriteLine($"Il costo complessivo è {dist[g[nodo_arrivo]]}");
+            }
+            else
+                Console.WriteLine($"Non esiste un percorso fra '{nodo_partenza}' e '{nodo_arrivo}'");
+        }
+    }
+}
