6077375bf5e3a1655113203921d59673c16d4d2a
Antostarwars <derosaantonio587@gmail.com>
2024-11-26 11:47:50 +0100

    Added Functions to AcquarioWPF

--- 4H/Informatica/AcquarioWPF/AquarioWPF/MainWindow.xaml.cs
+++ 4H/Informatica/AcquarioWPF/AquarioWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
@@ -12,12 +13,13 @@ namespace AquarioWPF
         private const int TICK_MILLISECONDS = 1000;
         DispatcherTimer timer;
         private int numeroPesci = 0;
+        private Image currentFish;
 
         public MainWindow()
         {
             InitializeComponent();
             SetupTimer();
-            AddFish(new Uri("pack://application:,,,/Images/fish-1.png"));
+            AddFish(new Uri("pack://application:,,,/Images/fish-2.png"));
         }
 
         private void SetupTimer()
@@ -39,6 +41,8 @@ namespace AquarioWPF
             Random random = new Random();
             Image img = new Image();
             img.Source = new BitmapImage(imgPath);
+            img.Height = 100;
+            img.Width = 100;
 
             // Randomizza la posizione dell'immagine inserita
             double posX = random.NextDouble() * CanvasAcquario.ActualWidth;
@@ -46,6 +50,37 @@ namespace AquarioWPF
             img.Margin = new Thickness(posX, posY, 0, 0);
 
             CanvasAcquario.Children.Add(img);
+        }
+
+
+        int x = 0;
+        int y = 0;
+        double scalaX = 1.0;
+        double scalaY = 1.0;
+        int gradi = 0;
+        private void TranslateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Image fish = CanvasAcquario.FindName("immaginePesceDefault") as Image ?? throw new Exception("Image not exists");
+
+            TranslateTransform translate = new TranslateTransform(--x, --y);
+            fish.RenderTransform = translate;
+        }
+
+        private void RotateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Image fish = CanvasAcquario.FindName("immaginePesceDefault") as Image ?? throw new Exception("Image not exists");
+
+            gradi += 10;
+            RotateTransform rotate = new RotateTransform(gradi, 0, 0);
+            fish.RenderTransform = rotate;
+        }
+
+        private void ScaleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Image fish = CanvasAcquario.FindName("immaginePesceDefault") as Image ?? throw new Exception("Image not exists");
+
+            ScaleTransform scale = new ScaleTransform(scalaX++, scalaY++, 0, 0);
+            fish.RenderTransform = scale;
         }
     }
 }
