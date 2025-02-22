cde789c0a18d27ece97b47b916c28dc03a0df521
Antostarwars <derosaantonio587@gmail.com>
2025-02-22 10:55:18 +0100

    Added SocketTCP

---
+++ 4H/Sistemi E Reti/SocketTCP/ClientSocketTCP/ClientSocketTCP/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientSocketTCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ip = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ip, 11000);
+
+                Socket sender = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine($"Socket connected to {sender.RemoteEndPoint}");
+
+                    byte[] msg = Encoding.ASCII.GetBytes("CIAO CIAO <EOF>");
+
+                    int bytesSent = sender.Send(msg);
+
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine($"Echoed test = {Encoding.ASCII.GetString(bytes, 0, bytesRec)}");
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("\n Press any key to continue...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ 4H/Sistemi E Reti/SocketTCP/ServerSocketTCP/ServerTCP_RealTIme/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace SocketTCP_RealTIme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartServer();
+        }
+
+        public static void StartServer()
+        {
+            IPHostEntry host = Dns.GetHostEntry("10.73.0.21");
+
+            IPAddress ip = IPAddress.Parse("10.73.0.21"); //host.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ip, 11000);
+
+            try
+            {
+                Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                listener.Bind(localEndPoint);
+
+                listener.Listen(10);
+
+                Console.WriteLine("Waiting for a connection...");
+
+                Socket handler = listener.Accept();
+
+                string data = null;
+
+                byte[] bytes = null;
+
+                byte[] msg = null;
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    //if (data.IndexOf("<EOF>") > -1) break;
+                    if (data == "NIGGA<EOF>") break;
+
+                    msg = Encoding.ASCII.GetBytes("Balls and Mussels for everyone except Cecca!");
+                    handler.Send(msg);
+                }
+
+                Console.WriteLine($"Text received: {data}");
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
